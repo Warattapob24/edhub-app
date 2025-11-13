6d1a2ad1f3531e3ce748f9833268b925263eed8d
edhub-app <warattapob24@gmail.com>
2025-11-13 11:41:54 +0700

    Fix migration: skip existing classroom index

--- migrations/versions/a0507f182cdf_link_subunit_to_assessmenttopic.py
+++ migrations/versions/a0507f182cdf_link_subunit_to_assessmenttopic.py
@@ -38,7 +38,7 @@ def upgrade():
                existing_nullable=True)
 
     with op.batch_alter_table('classroom', schema=None) as batch_op:
-        batch_op.create_index(batch_op.f('ix_classroom_program_id'), ['program_id'], unique=False)
+        #batch_op.create_index(batch_op.f('ix_classroom_program_id'), ['program_id'], unique=False)
         batch_op.create_foreign_key(None, 'program', ['program_id'], ['id'])
         batch_op.create_foreign_key(None, 'room', ['room_id'], ['id'])
 
@@ -60,7 +60,7 @@ def downgrade():
     with op.batch_alter_table('classroom', schema=None) as batch_op:
         batch_op.drop_constraint(None, type_='foreignkey')
         batch_op.drop_constraint(None, type_='foreignkey')
-        batch_op.drop_index(batch_op.f('ix_classroom_program_id'))
+        #batch_op.drop_index(batch_op.f('ix_classroom_program_id'))
 
     with op.batch_alter_table('audit_log', schema=None) as batch_op:
         batch_op.alter_column('record_id',
